edce29bb51ae19ced6189f292d8569bb35cc0344
Miles Curry <currymiles@gmail.com>
2024-04-16 13:42:04 -0600

    Update build command to be much much better

--- build.py
+++ build.py
@@ -1,27 +1,80 @@
+import os
 import subprocess
 import argparse
+from pprint import pprint
+
+BASE_DIRECTORY = 'build'
+
+PRODUCT_NAME = "Space Game"
+DESCRIPTION = "Space Game created by Miles A. Curry"
+COMPANY_NAME = "Miles A. Curry"
+
+DATA_DIRECTORY_SPEC = "E:/Projects/SpaceGame/resources=resources"
 
 
-def get_current_version():
-    subprocess.run(["git", ])
-    version = 0.0
+def get_current_tag_with_rev():
+    result = subprocess.run(['git', 'describe', '--tags'], capture_output=True)
+    return result.stdout.decode('utf-8').strip()
+
+
+def get_current_tag_as_string():
+    result = subprocess.run(['git', 'describe', '--tags', '--abbrev=0'], capture_output=True)
+    version = result.stdout.decode('utf-8').strip()
     return version
 
-def get_current_revision():
-    revision = 00000
-    return revision
 
-def construct_file_name(version, revision):
-    if revision:
-        filename = f"space.{version}.{revision}.exe"
+def get_current_tag_as_float():
+    result = subprocess.run(['git', 'describe', '--tags', '--abbrev=0'], capture_output=True)
+    version = result.stdout.decode('utf-8').strip()
+    version = float(version.removeprefix('v'))
+    return version
 
-directory = './build'
-filaneme = 'space.exe'
 
-subprocess.run(['python3.11.exe', '-m', 'nuitka',
-                '--product-name="Space Game"',
-                '--product-version=0.2',
-                './space.py',
-                '--standalone',
-                '--include-data-dir=E:/Projects/SpaceGame/resources=resources'
-                ])
+def construct_filename():
+    return f"space.{get_current_tag_with_rev()}.exe"
+
+
+def get_or_construct_build_directory():
+    if not os.path.isdir(BASE_DIRECTORY):
+        os.mkdir(BASE_DIRECTORY)
+
+    # The Build version directory i.e. build/v0.2
+    ver_dir = os.path.join(BASE_DIRECTORY, get_current_tag_as_string())
+    if not os.path.isdir(ver_dir):
+        os.mkdir(ver_dir)
+
+    # Now create the revision build directory build/v0.2/rev
+    ver_rev_dir = os.path.join(ver_dir, get_current_tag_with_rev())
+    if not os.path.isdir(ver_rev_dir):
+        os.mkdir(ver_rev_dir)
+
+    return ver_rev_dir
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-d', '--dryrun',
+                    help="Just print the constructed nuitka build command",
+                    action='store_true',
+                    default=False)
+
+args = parser.parse_args()
+
+nuitka_command = [
+    'python3.11.exe', '-m', 'nuitka',
+    f'--product-name={PRODUCT_NAME}',
+    f'--product-version={get_current_tag_as_float()}',
+    f'--include-data-dir={DATA_DIRECTORY_SPEC}',
+    f'--file-description={DESCRIPTION}',
+    f'--company-name={COMPANY_NAME}',
+    f'--output-filename=space.exe',
+    f'--output-dir={get_or_construct_build_directory()}',
+    '--remove-output',
+    './space.py',
+    '--standalone'
+]
+
+print("Nuitka Command to run:")
+pprint(nuitka_command)
+
+if not args.dryrun:
+    subprocess.run(nuitka_command)
